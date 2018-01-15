dcdeffeca6f3ae336cc73360c0ad9a92ab296b3e
thanhtrinh544 <32472630+thanhtrinh544@users.noreply.github.com>
2018-01-15 09:24:15 +0700

    file kết nối với mysql

---
+++ createpost.php
@@ -0,0 +1,15 @@
+ <?php
+
+
+ 		$servername = "localhost";
+ 		$username = "root";
+ 		$password = "";
+ 		$dbname = "pikashipper";
+
+
+ 		new mysqli($servername, $username, $password, $dbname);
+
+
+ 		echo "Connected successfully";
+
+ ?> 
